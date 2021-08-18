506252f17b92c38f0372fdba2c83f14ebe2ced86
roy-jingyang <7447643+roy-jingyang@users.noreply.github.com>
2021-08-18 17:23:32 +1000

    test pydata theme

--- source/conf.py
+++ source/conf.py
@@ -52,7 +52,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
+
 html_theme_options = {
     # outlook
     'show_prev_next': False,
